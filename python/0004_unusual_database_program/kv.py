a0312447d722de3dd94280de1a1d16f636449582
xMez <albin.soderstrom@hotmail.com>
2022-10-21 19:37:19 +0200

    Feature unusual database program (#3)

---
+++ python/0004_unusual_database_program/kv.py
@@ -0,0 +1,50 @@
+import asyncio
+import logging
+import logging.config
+from asyncio.transports import DatagramTransport
+
+# create logger
+logging.config.fileConfig("../logging.conf")
+logger = logging.getLogger("protohackers")
+
+
+class KvServerProtocol:
+    def __init__(self) -> None:
+        self.kv = {
+            "version": "Mez's Key-Value Store 0.1.0"
+        }
+
+    def connection_made(self, transport: DatagramTransport):
+        self.transport = transport
+
+    def datagram_received(self, data: bytes, addr):
+        message = data.decode()
+        command = message.split("=", 1)
+        if len(command) == 1:
+            key = command[0]
+            value = self.kv.get(key, "")
+            response = f"{key}={value}"
+            self.transport.sendto(response.encode(encoding="ascii"), addr)
+        elif len(command) == 2:
+            key, value = command[0], command[1]
+            if key == "version":
+                return
+            self.kv[key] = value
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    print("Starting UDP server")
+
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: KvServerProtocol(), local_addr=("0.0.0.0", 10007)
+    )
+
+    try:
+        await asyncio.sleep(3600)
+    finally:
+        transport.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main(), debug=True)
